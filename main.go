40f593381cff99fc296c8affcdf5082b489f770d
Maxence Vast <34278027+MaxVast@users.noreply.github.com>
2024-09-09 14:15:37 +0200

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/MaxVast/go-rest-api-mongodb/database"
 	"github.com/MaxVast/go-rest-api-mongodb/routes"
 	"github.com/joho/godotenv"
+	"github.com/rs/cors"
 	"log"
 	"net/http"
 )
@@ -15,10 +16,21 @@ func main() {
 		log.Fatalf("Erreur lors du chargement du fichier .env : %v", err)
 	}
 
+	//Init CORS
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000", "http://localhost:3000/"},
+		AllowedMethods:   []string{"GET"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization", "Accept"},
+		AllowCredentials: true,
+	})
+
 	// Init connection to BDD
 	database.Connect()
 
 	// Init routes API
 	router := routes.InitializeRoutes()
-	log.Fatal(http.ListenAndServe(":8000", router))
+	//Apply CORS to ROUTER
+	handler := c.Handler(router)
+
+	log.Fatal(http.ListenAndServe("127.0.0.1:8000", handler))
 }
