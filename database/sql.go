eaf24aec2865b2dcf96d7eb981a122e67cd409fd
Maxence Vast <34278027+MaxVast@users.noreply.github.com>
2024-09-10 10:48:51 +0200

    Add files via upload

---
+++ database/sql.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/denisenkom/go-mssqldb"
+	"log"
+	"os"
+)
+
+var DB *sql.DB
+
+func ConnectSql() {
+	sqlsvr := os.Getenv("DATABASE_URL")
+
+	if sqlsvr == "" {
+		log.Fatal("DATABASE_URL not defined in .env file")
+	}
+
+	var err error
+	DB, err = sql.Open("sqlserver", sqlsvr)
+	if err != nil {
+		log.Fatalf("Error connecting to SQL Server: %v", err)
+	}
+
+	// Check connexion
+	if err = DB.Ping(); err != nil {
+		log.Fatalf("Error checking connection to SQL Server: %v", err)
+	}
+
+	fmt.Println("Connected to SQL Server.")
+}
